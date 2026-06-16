38efcd71df23c9b6baf3bc0d72bb956ea05d4cb8
Preetham00x <preetham2006@outlook.com>
2026-06-17 02:57:11 +0530

    adding css and ui along with the jstl dependency

--- src/main/java/com/telidu/WebbAppCRM/model/Customer.java
+++ src/main/java/com/telidu/WebbAppCRM/model/Customer.java
@@ -1,5 +1,55 @@
 package com.telidu.WebbAppCRM.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
 public class Customer {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+	private String firstname;
+	private String lastname;	
+	private String city;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", firstname=" + firstname + ", lastname=" + lastname + ", city=" + city + "]";
+	}
+	public Customer(Integer id, String firstname, String lastname, String city) {
+		super();
+		this.id = id;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.city = city;
+	}
+	public Customer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 }
--- src/main/java/com/telidu/WebbAppCRM/web/CustomerController.java
+++ src/main/java/com/telidu/WebbAppCRM/web/CustomerController.java
@@ -1,12 +1,25 @@
 package com.telidu.WebbAppCRM.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.telidu.WebbAppCRM.model.Customer;
 import com.telidu.WebbAppCRM.service.ICustomerService;
 
 @Controller
 public class CustomerController {
 @Autowired
 private ICustomerService service;
+@GetMapping("/cx-info")
+public String getCxdata(Model model) {
+	List<Customer> customers=service.getCustomerInfo();
+	model.addAttribute("customers",customers);
+	customers.forEach(v->System.out.println(v));//debugging
+	return "customerinfo";
+	
+}
 }
