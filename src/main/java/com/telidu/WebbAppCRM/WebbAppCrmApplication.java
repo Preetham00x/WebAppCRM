98f85d774b40f3e018f99352ffe30224250a2741
Preetham00x <preetham2006@outlook.com>
2026-06-17 00:46:54 +0530

    implementation of SpringMVC in WebApp

---
+++ src/main/java/com/telidu/WebbAppCRM/WebbAppCrmApplication.java
@@ -0,0 +1,13 @@
+package com.telidu.WebbAppCRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbAppCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebbAppCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telidu/WebbAppCRM/WebbAppCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telidu.WebbAppCRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbAppCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
